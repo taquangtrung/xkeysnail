3ec2311a27401344d52eaba5b267c0be5d67d334
Ta Quang Trung <taquangtrungvn@gmail.com>
2025-05-22 14:44:45 +0800

    catch exception when checking if Rofi is running

--- xkeysnail/transform.py
+++ xkeysnail/transform.py
@@ -18,17 +18,17 @@ def get_active_window_wm_class(display=Xlib.display.Display()):
     current_window = display.get_input_focus().focus
 
     # TRUNG: Hack to check if Rofi is running and return the WM_CLASS of Rofi
-    # Find the Rofi window from the root window
-    root_window = current_window.query_tree().root
-    if root_window:
-        for child_window in root_window.query_tree().children:
-            if child_window:
-                try:
-                    wmclass = child_window.get_wm_class()
-                    if wmclass and "Rofi" in wmclass:
-                        return "Rofi"
-                except:
-                    pass
+    try:
+        # Find the Rofi window from the root window
+        root_window = current_window.query_tree().root
+        if root_window:
+            for child_window in root_window.query_tree().children:
+                if child_window:
+                        wmclass = child_window.get_wm_class()
+                        if wmclass and "Rofi" in wmclass:
+                            return "Rofi"
+    except:
+        pass
 
     # Otherwise, find WM_CLASS of the current windows as usual
     pair = get_class_name(current_window)
